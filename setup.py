6b6fe0b3aefe7b14322adbfa0705da1f767c6e2c
ahmedreza1 <ahmedreza80@gmail.com>
2023-09-19 16:39:07 +0530

    Added setup file to turn EasyGraph into a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EasyGraph',
+    version='0.1.0',
+    url='https://github.com/ahmedreza1/EasyGraph',
+    author='Md Ahmed Reza',
+    author_email='ahmedreza80@gmail.com',
+    description='A simple python module to creath all types of graphs.',
+    packages=find_packages(),    
+    install_requires=['matplotlib', 'pandas'],
+)
