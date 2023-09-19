5e3648444860a36b3079232991694a19c798c5ee
ahmedreza1 <ahmedreza80@gmail.com>
2023-09-19 17:20:47 +0530

    added long description for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+# Read the content of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='EasyGraph_py',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/ahmedreza1/EasyGraph',
     author='Md Ahmed Reza',
     author_email='ahmedreza80@gmail.com',
     description='A simple python module to create all types of graphs.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),    
     install_requires=['matplotlib', 'pandas'],
 )
