ce5d5835cca33cdeb9c4ddd54ce0438a64d120dc
ahmedreza1 <ahmedreza80@gmail.com>
2023-09-19 16:42:16 +0530

    Fixed a trypo in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='https://github.com/ahmedreza1/EasyGraph',
     author='Md Ahmed Reza',
     author_email='ahmedreza80@gmail.com',
-    description='A simple python module to creath all types of graphs.',
+    description='A simple python module to create all types of graphs.',
     packages=find_packages(),    
     install_requires=['matplotlib', 'pandas'],
 )
