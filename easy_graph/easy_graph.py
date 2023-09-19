4361d06b1382bf01b0a115a1f26daac9c9076cab
ahmedreza1 <ahmedreza80@gmail.com>
2023-09-19 17:24:39 +0530

    Added Scatter Plot Chart Type

--- easy_graph/easy_graph.py
+++ easy_graph/easy_graph.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 def bar_chart(data, x_col, y_col, title='Bar Chart', xlabel='X-axis', ylabel='Y-axis'):
     """Generate a bar chart from a pandas DataFrame."""
@@ -17,4 +18,14 @@ def line_chart(data, x_col, y_col, title='Line Chart', xlabel='X-axis', ylabel='
     plt.ylabel(ylabel)
     plt.show()
 
-# Additional functions for other chart types (e.g., scatter_plot, histogram) can be added here.
+def scatter_plot(data, x_col, y_col, title='Scatter Plot', xlabel='X-axis', ylabel='Y-axis'):
+    """Generate a scatter plot from a pandas DataFrame."""
+    plt.scatter(data[x_col], data[y_col])
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+
+
+# Additional functions for other chart types can be added here.
